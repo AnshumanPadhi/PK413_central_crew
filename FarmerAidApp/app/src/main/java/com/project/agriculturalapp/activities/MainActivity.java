2929a615c7220dce1a3ac69623ce460035f426a6
ishanvohra2 <ishanvohra2@gmail.com>
2020-08-02 14:05:20 +0530

    Farmers app added

---
+++ FarmerAidApp/app/src/main/java/com/project/agriculturalapp/activities/MainActivity.java
@@ -0,0 +1,92 @@
+package com.project.agriculturalapp.activities;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.project.agriculturalapp.R;
+import com.project.agriculturalapp.adapters.MainAdapter;
+import com.project.agriculturalapp.modals.MainListItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<MainListItem> list;
+    private RecyclerView recyclerView;
+    private MainAdapter adapter;
+
+    private Integer[] imageUrls={R.raw.crop_production_opt,R.raw.treat,R.raw.govp};
+    private Integer[] hindiTexts={R.string.crop_production_card_title_hi,R.string.treatment_card_title_hi,R.string.policy_card_title_hi};
+    private Integer[] englishTexts={R.string.crop_production_card_title_en,R.string.treatment_card_title_en,R.string.policy_card_title_en};
+    private String[] backgroundColors={"#35e372","#a4f075","#ff9f80"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent[] links={
+                new Intent(MainActivity.this, CropProductionActivity.class),
+                new Intent(MainActivity.this, SelectProblem.class),
+                new Intent(MainActivity.this, Select_Policy.class)
+        };
+
+        list = new ArrayList<>();
+        for(int i=0;i<imageUrls.length;i++){
+            MainListItem item=new MainListItem();
+
+            item.setImageUrl(imageUrls[i]);
+            item.setHindiText(hindiTexts[i]);
+            item.setEnglishText(englishTexts[i]);
+            item.setBackgroundColor(backgroundColors[i]);
+            item.setIntent(links[i]);
+
+            list.add(item);
+        }
+
+        adapter = new MainAdapter(this,list);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        Log.v("version", Build.VERSION.SDK_INT + "");
+
+        findViewById(R.id.progress).setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            Intent a = new Intent(Intent.ACTION_MAIN);
+            a.addCategory(Intent.CATEGORY_HOME);
+            a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(a);
+        }
+    }
+}
